c6d7b8002ef801ead0a2cbfc60647278bd027409
Ravan <ravan2003@gmail.com>
2024-02-20 23:59:16 +0800

    Finished

---
+++ signup-form/main.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("#signup");
+const firstName = form.querySelector("input[name='first-name']");
+const lastName = form.querySelector("input[name='last-name']");
+const email = form.querySelector("input[name='email']");
+const phone = form.querySelector("input[name='phone']");
+const password = form.querySelector("input[name='password']");
+const confirmPwd = form.querySelector("input[name='confirm-pwd']");
+const passwordMessage = form.querySelector(".password-message");
+const submit = form.querySelector(".submit")
+
+
+
+function nameValidation(name) {
+    let re = /[a-zA-z]+/;
+    if (!re.test(name)) {
+        return true
+    }
+}
+
+function numberValidation(number) {
+    let re = /\+?\d{7,10}/;
+    if (!re.test(number)) {
+        return true
+    }
+}
+
+function emailValidation(email) {
+    let re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!re.test(email)) {
+        return true
+    }
+}
+
+submit.addEventListener("click", (e) => {
+    console.log('submit')
+    if (nameValidation(firstName.value)) {
+        firstName.classList.add("error");
+    }
+    if (nameValidation(lastName.value)) {
+        lastName.classList.add("error");
+    }
+    if (numberValidation(phone.value)) {
+        phone.classList.add("error");
+    }
+    if (emailValidation(email.value)) {
+        email.classList.add("error");
+    }
+    if (password.value != confirmPwd.value) {
+        password.classList.add("error");
+        confirmPwd.classList.add("error");
+        passwordMessage.textContent = "* Passwords do not match"
+    }
+    e.preventDefault();
+});
+
